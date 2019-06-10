a8fc1a05981d76f306e7e25cca1881317bc9b64d
mary <fakemariastyles>
2019-06-10 16:36:33 +0430

    added test for local data source

--- app/src/androidTest/java/com/workshop/aroundme/local/datasource/UserLocalDataSourceTest.kt
+++ app/src/androidTest/java/com/workshop/aroundme/local/datasource/UserLocalDataSourceTest.kt
@@ -5,8 +5,12 @@ import android.content.SharedPreferences
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.workshop.aroundme.data.model.UserEntity
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @MediumTest
@@ -20,6 +24,20 @@ class UserLocalDataSourceTest {
         val context = ApplicationProvider.getApplicationContext<Context>()
         sharedPreferences = context.getSharedPreferences("user.data", Context.MODE_PRIVATE)
         userLocalDataSource = UserLocalDataSource(sharedPreferences)
+    }
+
+    @Test
+    fun return_empty_when_not_login(){
+        val user = userLocalDataSource.getUser()
+        assertNull(user)
+    }
+
+    @Test
+    fun returns_user_after_login(){
+        val user = UserEntity("mary")
+        userLocalDataSource.login(user)
+        val result = userLocalDataSource.getUser()
+        assertEquals(result , user)
     }
 
     @After
