2f4b25055d52f050b0753f7b8d23cf814c1d805f
mary <fakemariastyles>
2019-06-11 14:18:07 +0430

    SignleUseCaseAdded

--- app/src/main/java/com/workshop/aroundme/domain/intractor/base/SingleUseCase.kt
+++ app/src/main/java/com/workshop/aroundme/domain/intractor/base/SingleUseCase.kt
@@ -1,5 +1,29 @@
 package com.workshop.aroundme.domain.intractor.base
 
-abstract class SingleUseCase {
+import com.workshop.aroundme.domain.executer.UseCaseExecuterThread
+import io.reactivex.Single
+
+abstract class SingleUseCase<in Params, Result>
+    (
+    private val postUseCaseExecuterThread: UseCaseExecuterThread
+    , private val useCaseExecuterThread: UseCaseExecuterThread
+) : BaseUseCase() {
+    abstract fun buildSingle(params: Params): Single<Result>
+    fun execute(
+        params: Params
+        , success: (Result) -> Unit
+        , failure: (Throwable) -> Unit
+    ) {
+        buildSingle(params)
+            .subscribeOn(useCaseExecuterThread.scheduler)
+            .observeOn(postUseCaseExecuterThread.scheduler)
+            .subscribe({
+                success(it)
+            }, {
+                failure(it)
+            }).also {
+                add(it)
+            }
+    }
 
 }
